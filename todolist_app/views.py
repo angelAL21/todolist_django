5d2e365eca57e3befe412ba3af4d12b9f1d2448d
unknown <angell_2104@hotmail.com>
2022-01-31 18:56:01 -0600

    coded the contact, about and todolist pages

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def todolist(request):
-    return HttpResponse("welcome to task page")
+    context = {
+        'welcome_text': "welcome ToDo list page",
+        }
+    return render(request, 'todolist.html', context)
+
+def contact(request):
+    context = {
+        'contact_text': "welcome to contact page.",
+        }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        'about_text': "welcome to about page.",
+        }
+    return render(request, 'about.html', context)
