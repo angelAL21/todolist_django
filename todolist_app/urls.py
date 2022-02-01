fb91a1bd4f37a3175f1f874d4235fa7cd2b0f6fb
unknown <angell_2104@hotmail.com>
2022-01-31 18:56:58 -0600

    the path contact and about were created

--- todolist_app/urls.py
+++ todolist_app/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.todolist, name= 'todolist'),
+    path('contact', views.contact, name= 'contact'),
+    path('about', views.about, name= 'about'),
 ]
