7eed73638b5ce48ab843459699e54856f1bf329f
unknown <angell_2104@hotmail.com>
2022-02-10 17:45:02 -0600

    we coded the delete func of one task. also comment the other funcs

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -3,7 +3,7 @@ from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
 from django.contrib import messages
 
-# Create your views here.
+# adding and showing tasks.
 def todolist(request):
     if request.method == "POST":
         form= TaskForm(request.POST or None)
@@ -15,14 +15,23 @@ def todolist(request):
         all_tasks  = TaskList.objects.all
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
+#contact page
 def contact(request):
     context = {
         'contact_text': "welcome to contact page.",
         }
     return render(request, 'contact.html', context)
 
+#about page
 def about(request):
     context = {
         'about_text': "welcome to about page.",
         }
     return render(request, 'about.html', context)
+
+#delete a task
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+    return redirect('todolist')
+    
