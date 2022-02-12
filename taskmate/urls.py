f9ce678ff84613a929b8c635783a1f01b17f6281
unknown <angell_2104@hotmail.com>
2022-02-12 11:43:19 -0600

    added the path about and contact from todolist_app/urls.py because we want to rout them directly and not from /task

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from todolist_app import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('task/', include('todolist_app.urls')), #this redirects to our todolist_app urls funct
+    path('contact', views.contact, name= 'contact'),
+    path('about', views.about, name= 'about'),
+    path('', views.index, name="index"),
 ]
